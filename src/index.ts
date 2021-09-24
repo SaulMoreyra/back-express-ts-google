d1359d402d198fc6f2604e0a92b5201555486319
SaulMoreyra <armhacam21@gmail.com>
2021-09-24 16:12:11 -0500

    Public index was added ✅

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import dotenv from "dotenv";
 dotenv.config({ path: ".env" });
 
@@ -10,6 +11,13 @@ const port = Number(process.env.PORT) || 8081;
 
 app.use(cors());
 app.set("port", port);
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(/^\/(?!api).*/, (req, res) => {
+  const indexPath = path.join(__dirname, "..", "public", "index.html");
+  res.sendFile(indexPath);
+});
+
 app.use("/api", routes);
 
 app.listen(port, () => {
